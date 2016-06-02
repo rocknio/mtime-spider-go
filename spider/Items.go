23cd71e05f4903977400862316101a81cb6b459d
rocknio@gmail.com <rocknio@gmail.com>
2016-06-02 10:34:19 +0800

    增加基础数据结构

--- spider/Items.go
+++ spider/Items.go
@@ -1,1 +1,13 @@
 package spider
+
+type Item struct {
+	Ranking    int
+	Id         int
+	Title      string
+	Link       string
+	Directors  string
+	Actors     string
+	image_urls []string
+}
+
+type Items []Item
--- spider/spider.go
+++ spider/spider.go
@@ -1,1 +1,36 @@
 package spider
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	uninit = iota
+	inited
+	started
+	end
+)
+
+type Spider struct {
+	start_urls        []string
+	start_task_count  int
+	finish_task_count int
+	status            int
+}
+
+type Spider_info Spider
+
+func (s *Spider) init() (err error) {
+	if s.status != uninit {
+		err_string := fmt.Printf("Spider is already inited!, Spider status is %+v", s.status)
+		return errors.New(err_string)
+	}
+
+	s.start_urls = []string{}
+	s.status = inited
+}
+
+func (s *Spider) set_start_urls(err error) {
+
+}
